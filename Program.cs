4fc5a7f927acfdd3b80688039266669553013711
Eric Huang <ericshuang23@gmail.com>
2019-01-21 18:26:22 -0800

    refactor FizzBuzz and added Pamplemousse

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press 1 to play FizzBuzz. Press 2 to play Pamplemousse.");
+            Console.Write("What game would you like to play: ");
+            int game = int.Parse(Console.ReadLine());
+
             int startingNumber = 1;
             Console.Write("How many numbers of output would you like: ");
             double endingNumber = double.Parse(Console.ReadLine());
@@ -19,10 +23,34 @@ namespace FizzBuzz
                 Console.WriteLine($"Please choose a number between 1 and {int.MaxValue} next time.");
                 endingNumber = 20;
             }
-            int outputFizz = 3;
-            int outputBuzz = 5;
- 
-            for(int i = startingNumber; i <= endingNumber; i++)
+
+            switch (game)
+            {
+                case 1:
+                    int outputFizz = 3;
+                    int outputBuzz = 5;
+                    FizzBuzz(startingNumber, Convert.ToInt32(endingNumber), outputFizz, outputBuzz);
+                    break;
+                case 2:
+                    int value = 4; //the number we want to check against.
+                    string pamplemousse = "Pamplemousse";
+                    Pamplemousse(startingNumber, Convert.ToInt32(endingNumber), value, pamplemousse);
+                    break;
+                default:
+                    Console.WriteLine("That number is not one of the options.");
+                    Console.WriteLine("Please press 1 to play FizzBuzz or press 2 to play Pamplemousse.");
+                    //TODO: add loop so there can be reselection
+                    break;
+            }
+            
+            //Used to keep the console on screen and text readable
+            Console.ReadLine();
+        }
+
+
+        static void FizzBuzz(int start, int end, int outputFizz, int outputBuzz)
+        {
+            for (int i = start; i <= end; i++)
             {
                 if ((i % outputFizz == 0) && (i % outputBuzz == 0))
                 {
@@ -41,9 +69,23 @@ namespace FizzBuzz
                     Console.WriteLine(i);
                 }
             }
-     
-            //Used to keep the console on screen and text readable
-            Console.ReadLine();
         }
+
+        static void Pamplemousse(int start, int end, int value, string pamplemousse)
+        {
+            string valueString = value.ToString();
+            for(int i = start; i <= end; i++)
+            {
+                string intString = i.ToString();
+                if ((intString.Contains(valueString)) || (i % value == 0)){
+                    Console.WriteLine(pamplemousse);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -1,12 +1,19 @@
-# FizzBuzz
-Plays the classic interview question/children's game of FizzBuzz.<br>
+## FizzBuzz
+The classic interview question/children's game of FizzBuzz <br>
 Two people count up in turn starting from 1.<br>
 However, if the number is divisible by 3, you say *Fizz* instead. Also, if the number is divisible by 5, you say *Buzz* instead. <br>
 Finally, if the number is divisible by both 3 and 5, you say *FizzBuzz*. <br>
 
-## Sample Output
-The following is first 20 numbers of FizzBuzz. The output could continue up to 2147483647 (the max int value) if desired. <br>
-**1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz**
+## Pamplemousse
+Pamplemousse is an alternate version of FizzBuzz. Named after what you say when the conditions are met.<br>
+Side Note: Pamplemousse is grapefruit in French.<br>
+Two people count up in turn starting from 1. <br>
+However, if the number is divisible by 4 or contains 4, you say *Pamplemousse* instead.
+
+### Sample Output
+The following is first 20 numbers of both games. The output could continue up to 2147483647 (the max int value) if desired. <br>
+**FizzBuzz Output:** 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz <br>
+**Pamplemousse Output:** 1 2 3 Pamplemousse 5 6 7 Pamplemousse 9 10 11 Pamplemousse 13 Pamplemousse 15 Pamplemousse 17 18 19 Pamplemousse
 
 ## Context
 Used to learn the basics of C#.
